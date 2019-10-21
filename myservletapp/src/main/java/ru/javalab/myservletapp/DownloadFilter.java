90efee772fcb62e23e5f32f9202ab1fc6de1c4cc
BLG51 <zary1998@gmail.com>
2019-10-21 15:27:40 +0300

    servletapp

--- myservletapp/src/main/java/ru/javalab/myservletapp/DownloadFilter.java
+++ myservletapp/src/main/java/ru/javalab/myservletapp/DownloadFilter.java
@@ -1,24 +1,30 @@
+package ru.javalab.myservletapp;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class MyFilter implements Filter {
+public class DownloadFilter implements Filter {
+    @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
     }
 
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpReq = (HttpServletRequest) servletRequest;
-        String methpd = httpReq.getMethod();
-        File file = new File("myfile.txt");
-        PrintWriter pw = new PrintWriter(file);
-        pw.println(methpd);
+        String method = httpReq.getMethod();
+        File file = new File("downloaderlogs.txt");
+        PrintWriter pw = new PrintWriter(new FileWriter(file, true));
+        pw.println(method);
         pw.flush();
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
+    @Override
     public void destroy() {
 
     }
--- myservletapp/src/main/java/ru/javalab/myservletapp/GreetingServlet.java
+++
@@ -1,4 +0,0 @@
-package ru.javalab.myservletapp;
-
-public class GreetingServlet {
-}
